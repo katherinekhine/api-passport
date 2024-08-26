e4c0e155258ebd39a5cf401c485996fd74e2c6d8
katherinekhine <khaingkatherine@gmail.com>
2024-08-26 14:42:25 +0630

    add register and login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
-        Passport::hashClientSecrets();
+        // Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
+        // Passport::hashClientSecrets();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/auth/register', [RegisterController::class, 'register']);
+Route::post('/auth/login', [LoginController::class, 'login']);
